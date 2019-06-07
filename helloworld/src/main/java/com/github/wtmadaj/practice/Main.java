4257989bb1a8a5ee0acd00e35a6d7f23a04022df
wtmadaj <wtmadaj@users.noreply.github.com>
2019-06-07 16:05:03 -0400

    Minor edits

---
+++ helloworld/src/main/java/com/github/wtmadaj/practice/Main.java
@@ -0,0 +1,14 @@
+package com.github.wtmadaj.practice;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main (String[] args) {
+
+        System.out.println("Testing 1, 2, 3");
+
+    }
+}
